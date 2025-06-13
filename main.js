e8c2df6b4a5ec1a6d177ef4a7ffb7108aa549d8a
dadozxdelax <138135966+dadozxdelax@users.noreply.github.com>
2025-06-13 08:37:31 -0300

    ok

--- main.js
+++ main.js
@@ -16,16 +16,9 @@ textos[j].classList.remove("ativo");
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-20T00:00:00");
-const tempoObjetivo2 = new Date("2026-01-01T00:00:00");
-const tempoObjetivo3 = new Date("2026-03-01T00:00:00");
-const tempoObjetivo4 = new Date("2027-01-01T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function atualizaCronometro(){
-for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
+contadores[0].textContent=calculaTempo(tempoObjetivo1)
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date()
@@ -35,9 +28,9 @@ function calculaTempo(tempoObjetivo) {
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-    segundos %= 60;
+     segundos %= 60;
     minutos %=60;
-    horas %=24;
+    horas %=24; 
 
-    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
